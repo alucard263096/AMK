b330e4143366d81b6e689aba3db59a2efcb52c15
alucard263096 <alucard263096@126.com>
2015-08-29 01:14:54 +0800

    API create and have banner as example

---
+++ API/classes/datamgr/banner.cls.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * Created on 2011-2-7
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */  
+ class BannerMgr
+ {
+ 	private static $instance = null;
+	public static $dbmgr = null;
+	public static $webServiceClient = null;
+	public static function getInstance() {
+		return self :: $instance != null ? self :: $instance : new BannerMgr();
+	}
+
+	private function __construct() {
+		
+	}
+	
+	public  function __destruct ()
+	{
+		
+	}
+	
+	public function getBannerListByCondition($condition,$lastupdate_time)
+	{
+		$condition=parameter_filter($condition);
+		$lastupdate_time=parameter_filter($lastupdate_time);
+		$sql="select * from tb_banner where code like '$condition'  ";
+		if($lastupdate_time!=""){
+		$sql.=" and updated_date>'$lastupdate_time'  ";
+		}
+		//echo $sql;
+		$query = $this->dbmgr->query($sql);
+		$result = $this->dbmgr->fetch_array_all($query); 
+		return $result;
+	}
+
+	
+ }
+ 
+ $bannerMgr=BannerMgr::getInstance();
+ $bannerMgr->dbmgr=$dbmgr;
+ 
+ 
+ 
+ 
+?>
