35f686eed655fb2027ae2d36f2940c8dbe11c953
Steve <alucard263096@126.com>
2015-09-27 16:49:45 +0800

    Commit

--- API/Order/charchat_update.php
+++ API/Order/charchat_update.php
@@ -2,13 +2,13 @@
   require '../include/common.inc.php';
   include ROOT.'/classes/datamgr/order.cls.php';
 
-  $order=$_REQUEST["order"];
+  $order_id=$_REQUEST["order_id"];
   $doctor_id=$_REQUEST["doctor_id"];
   $sendside=$_REQUEST["sendside"];
   $type=$_REQUEST["type"];
   $content=$_REQUEST["content"];
   
-  $result=$orderMgr->addContentToCharchat($order,$doctor_id,$sendside,$type,$content);
+  $result=$orderMgr->addContentToCharchat($order_id,$doctor_id,$sendside,$type,$content);
 
   outputXml($result);
 
