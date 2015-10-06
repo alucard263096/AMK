d0c36064e4858414f42ca627a04acbe7aa5759f4
alucard263096 <alucard263096@126.com>
2015-10-06 18:37:41 +0800

    完成大厦board日常统计

--- DoctorWeb/Admin/dashboard.php
+++ DoctorWeb/Admin/dashboard.php
@@ -7,13 +7,20 @@
  */
   require '../include/common.inc.php';
   include ROOT.'/include/init.inc.php';
+  require ROOT.'/classes/datamgr/statistics.cls.php';
   
   $smarty->assign("MyModule","admin");
   $smarty->assign("MyMenuId","dashboard");
 
   $reminderList=$businessMgr->getReminderAll($SysUser["id"]);
-  
   $smarty->assign("ReminderList",$reminderList);
+  $smarty->assign("ReminderListCount",count($reminderList));
+
+  
+  $StatisticsItem=$statisticsMgr->getDataForDashboard($SysUser["id"]);
+  $smarty->assign("StatisticsItem",$StatisticsItem);
+  $smarty->assign("StatisticsItemCount",count($StatisticsItem));
+
 
   $smarty->display(ROOT.'/templates/dashboard.html');
   
