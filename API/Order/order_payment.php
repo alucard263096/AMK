5c61174b5bb069898a54144c21d993d45dbe6d91
alucard263096 <alucard263096@126.com>
2015-10-19 23:33:23 +0800

    fix bug for send sms

--- API/Order/order_payment.php
+++ API/Order/order_payment.php
@@ -8,13 +8,11 @@
   $member_id=$_REQUEST["member_id"];
   $payment_type=$_REQUEST["payment_type"];
   $result=$orderMgr->payment($order_id,$member_id,$payment_type);
-  outputXml($result);
 
   $order=$orderMgr->getOrder($order_id,$member_id);
-  if(count($order)>0&&$result[0]["id"]==0&&$result[0]["status"]=="P"){
+  if(count($order)>0&&$order[0]["status"]=="P"){
 	$noticeMgr->sendOrderForDoctor($order[0]);
 	$noticeMgr->sendOrderForClient($order[0]);
-
 	$smsMgr->SendQueryConfirm($order[0]["mobile"],$order[0]["tag_name"],$order[0]["order_date"]." ".$order[0]["order_time"]);
   }
   
