2ded63776d3f229366a76571ba04fe997b97b8ff
Steve Cai <steve.cai@morningstar.com>
2015-11-23 15:46:23 +0800

    Commit fix

--- API/Notice/list.php
+++ API/Notice/list.php
@@ -1,5 +1,5 @@
 <?php
-  require 'include/common.inc.php';
+  require '../include/common.inc.php';
   include ROOT.'/classes/datamgr/notice.cls.php';
   $doctor_id=$_REQUEST["doctor_id"];
   $last_time=$_REQUEST["last_time"];
