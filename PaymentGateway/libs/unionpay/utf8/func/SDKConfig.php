96787f7ecf79e389a098c688ddd46d27b3843ae2
alucard263096 <alucard263096@126.com>
2015-12-17 20:53:09 +0800

    commit for unionpay test

--- PaymentGateway/libs/unionpay/utf8/func/SDKConfig.php
+++ PaymentGateway/libs/unionpay/utf8/func/SDKConfig.php
@@ -2,10 +2,10 @@
  
 // ######(以下配置为PM环境：入网测试环境用，生产环境配置见文档说明)#######
 // 签名证书路径
-const SDK_SIGN_CERT_PATH = 'D:/certs/acp_test_sign.pfx';
+const SDK_SIGN_CERT_PATH = 'C:/AMK/certs/amika.pfx';
 
 // 签名证书密码
-const SDK_SIGN_CERT_PWD = '000000';
+const SDK_SIGN_CERT_PWD = 'amika';
 
 // 密码加密证书（这条一般用不到的请随便配）
 const SDK_ENCRYPT_CERT_PATH = 'D:/AMK/certs/acp_test_enc.cer';
@@ -35,10 +35,10 @@ const SDK_Card_Request_Url = 'https://101.231.204.80:5000/gateway/api/cardTransR
 const SDK_App_Request_Url = 'https://101.231.204.80:5000/gateway/api/appTransReq.do';
 
 // 前台通知地址 (商户自行配置通知地址)
-const SDK_FRONT_NOTIFY_URL = 'http://localhost:8085/upacp_sdk_php/demo/api_01_gateway/FrontReceive.php';
+const SDK_FRONT_NOTIFY_URL = 'http://www.myhkdoc.com/AMK/PaymentGateway/UnionPay/notify_url.php';
 
 // 后台通知地址 (商户自行配置通知地址，需配置外网能访问的地址)
-const SDK_BACK_NOTIFY_URL = 'http://222.222.222.222/upacp_sdk_php/demo/api_01_gateway/BackReceive.php';
+const SDK_BACK_NOTIFY_URL = 'http://www.myhkdoc.com/AMK/PaymentGateway/UnionPay/return.php';
 
 //文件下载目录 
 const SDK_FILE_DOWN_PATH = 'C:/AMK/file/';
