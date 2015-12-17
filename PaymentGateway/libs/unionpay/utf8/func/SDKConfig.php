581401214706fe79215e6528dbb1b34f047fd8ab
alucard263096 <alucard263096@126.com>
2015-12-17 21:12:58 +0800

    commit

--- PaymentGateway/libs/unionpay/utf8/func/SDKConfig.php
+++ PaymentGateway/libs/unionpay/utf8/func/SDKConfig.php
@@ -8,25 +8,25 @@ const SDK_SIGN_CERT_PATH = 'C:/AMK/certs/amika.pfx';
 const SDK_SIGN_CERT_PWD = 'amika';
 
 // 密码加密证书（这条一般用不到的请随便配）
-const SDK_ENCRYPT_CERT_PATH = 'C:/AMK/certs/acp_test_enc.cer';
+const SDK_ENCRYPT_CERT_PATH = 'C:/AMK/certs/acp_prod_enc.cer';
 
 // 验签证书路径（请配到文件夹，不要配到具体文件）
 const SDK_VERIFY_CERT_DIR = 'C:/AMK/certs/';
 
 // 前台请求地址
-const SDK_FRONT_TRANS_URL = 'https://101.231.204.80:5000/gateway/api/frontTransReq.do';
+const SDK_FRONT_TRANS_URL = 'https://gateway.95516.com/gateway/api/frontTransReq.do';
 
 // 后台请求地址
-const SDK_BACK_TRANS_URL = 'https://101.231.204.80:5000/gateway/api/backTransReq.do';
+const SDK_BACK_TRANS_URL = 'https://gateway.95516.com/gateway/api/backTransReq.do';
 
 // 批量交易
-const SDK_BATCH_TRANS_URL = 'https://101.231.204.80:5000/gateway/api/batchTrans.do';
+const SDK_BATCH_TRANS_URL = 'https://gateway.95516.com/gateway/api/batchTransReq.do';
 
 //单笔查询请求地址
-const SDK_SINGLE_QUERY_URL = 'https://101.231.204.80:5000/gateway/api/queryTrans.do';
+const SDK_SINGLE_QUERY_URL = 'https://gateway.95516.com/gateway/api/queryTrans.do';
 
 //文件传输请求地址
-const SDK_FILE_QUERY_URL = 'https://101.231.204.80:9080/';
+const SDK_FILE_QUERY_URL = 'https://filedownload.95516.com/';
 
 //有卡交易地址
 const SDK_Card_Request_Url = 'https://101.231.204.80:5000/gateway/api/cardTransReq.do';
