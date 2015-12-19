0441600e2cd1febd8b7f8733792b25d59231414a
steve.cai <alucard263096@126.com>
2015-12-19 22:52:16 +0800

    修改为关闭页面

--- PaymentGateway/UnionPay/return.php
+++ PaymentGateway/UnionPay/return.php
@@ -29,21 +29,7 @@ include_once ROOT . '/libs/unionpay/utf8/func/secureUtil.php';
 </head>
 <body>
 <script>
-function openApp() {  
-  
-        if (/android/i.test(navigator.userAgent)) {  
-             var isrefresh = getUrlParam('refresh'); // 获得refresh参数  
-             if(isrefresh == 1) {  
-                 return  
-             }  
-             window.location.href = 'myapp://amklovebaby/success?order_no=<?php echo $order_id; ?>';  
-             window.setTimeout(function () {  
-                     window.location.href += '&refresh=1' // 附加一个特殊参数，用来标识这次刷新不要再调用myapp:// 了  
-             }, 500);  
-         }  
-  
-}
-openApp();
+window.close();
 </script>
 </body>
 </html>
